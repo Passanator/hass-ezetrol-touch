bfba58e80b3f98d6fc60444af48a03ce21a79fab
Watch Software <CPASS>
2025-04-24 10:03:37 +0100

    updates

---
+++ custom_components/ezetrol_touch/const.py
@@ -0,0 +1,9 @@
+"""Constants for the Ezetrol Touch integration."""
+
+DOMAIN = "ezetrol_touch"
+
+# Configuration keys
+CONF_IP_ADDRESS = "ip_address"
+
+# Default update interval (5 minutes = 300 seconds)
+SCAN_INTERVAL = 300
